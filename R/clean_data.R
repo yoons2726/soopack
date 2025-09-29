cab4b4654fd7c48b38db0178a88938f87f6bcc66
Soohyun <yoons2726@gmail.com>
2025-09-29 13:00:14 -0400

    Update clean_data.R

--- R/clean_data.R
+++ R/clean_data.R
@@ -5,21 +5,22 @@
 #' @param value_name change name of value stored
 #' @author Soohyun Yoon
 #' @import dplyr
-#' @examples
-#' clean_data(maternal_mortality, "maternal_mortality")
+#' @examples clean_data(maternal_mortality, "maternal_mortality")
 #' @export
+#' @importFrom tidyr pivot_longer
+#' @importFrom dplyr mutate select
 
-library(dplyr)
+
 clean_data <- function(data, value_name) {
     data %>%
-    pivot_longer(
+    tidyr::pivot_longer(
       cols = starts_with("X"),
       names_to = "year",
       values_to = value_name,
       names_prefix = "X"
     ) %>%
-    mutate(
+    dplyr::mutate(
       year = as.numeric(year)
     ) %>%
-    select("iso", "year", value_name)
+    dplyr::select("iso", "year", all_of(value_name))
 }
